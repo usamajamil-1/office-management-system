4440280f70f74512006f47f8e233b27609331c17
Usama Jamil <usamajamil176@gmail.com>
2026-07-09 16:41:48 +0500

    Fix API URL

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://office-management-system-backend-m7u3.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
