9b23d86f0a0debdb30ce6075cd0a15c0cd12c7ab
Usama Jamil <usamajamil176@gmail.com>
2026-07-15 02:29:21 +0500

    chore: remove unused dummy user.js (dead code, not used anywhere)

--- src/data/user.js
+++
@@ -1,6 +0,0 @@
-export const users = [
-  { email: 'admin@gmail.com', password: '1234', role: 'admin' },
-  { email: 'hr@gmail.com', password: '1234', role: 'hr' },
-  { email: 'accounts@gmail.com', password: '1234', role: 'accountant' },
-  { email: 'employee@gmail.com', password: '1234', role: 'employee' },
-]
